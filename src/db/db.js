0deaee3ce94d6fdb90c7128b708b700fde516258
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2025-02-13 17:26:00 +0500

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -7,7 +7,7 @@ export class PostgresModel {
 
     constructor() {
         this.#pg = new pg.Pool({
-            connectionString:'postgresql://forpredictor_user:aOIgsXdrgJ08sHFbmn2A2rrBpAMgMeSV@dpg-cumtkmpu0jms73b86540-a.oregon-postgres.render.com/forpredictor'
+            connectionString:'postgresql://forpredictor_user:aOIgsXdrgJ08sHFbmn2A2rrBpAMgMeSV@dpg-cumtkmpu0jms73b86540-a/forpredictor'
                 // postgres://aevbuklb:R3WIbjMDum_lC4zc72WaBhYXpqaCJ3TQ@satao.db.elephantsql.com/aevbuklb
             
             
