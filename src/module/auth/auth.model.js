1facf18bdca662794ace2c685df43a5165ebff9d
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2025-03-24 01:01:31 +0500

    Update auth.model.js

--- src/module/auth/auth.model.js
+++ src/module/auth/auth.model.js
@@ -20,42 +20,34 @@ export class LoginModel {
         return data;
     }
 
- async signIn(id){
-        const data = await this.#db.fetch(`
-        SELECT * FROM login WHERE id = $1`,id)
 
+async signIn(id, lang) {
+    // Foydalanuvchini login jadvalidan tekshirish
+    const loginData = await this.#db.fetch(`
+        SELECT * FROM login WHERE id = $1`, [id]);
 
+    if (loginData.length > 0) {
+        // Agar foydalanuvchi login jadvalida bo'lsa, uni qaytarish
+        return loginData;
+    } else {
+        // Agar foydalanuvchi login jadvalida topilmasa, unlogin jadvalini tekshirish
+        const unloginData = await this.#db.fetch(`
+            SELECT * FROM unlogin WHERE id = $1`, [id]);
 
+        if (unloginData.length === 0) {
+            // Agar foydalanuvchi unlogin jadvalida ham bo'lmasa, uni unlogin jadvaliga qo'shish
+            await this.#db.execute(`
+                INSERT INTO unlogin (id, lang) 
+                VALUES ($1, $2)`, [id, lang]);
 
-        return data
-
-
-
-
-
-
-
-
-
-
-
+            return { status: 'User added to unlogin' };
+        } else {
+            // Agar foydalanuvchi unlogin jadvalida bo'lsa, hech narsa qilmaslik
+            return { status: 'User already exists in unlogin' };
+        }
     }
-    // if (data.length > 0) {
-    //     // Agar foydalanuvchi login jadvalida bo'lsa, uni qaytarish
-    //     return data
-    // } else {
-    //     // Agar foydalanuvchi login jadvalida topilmasa, unlogin jadvaliga qo'shish
-    //     await this.#db.execute(`
-    //         INSERT INTO unlogin (id, language) 
-    //         VALUES ($1, $2)`, [id, language]);
+}
 
-        // return {
-        //     message: "User inserted into unlogin table",
-        //     id,
-        //     language
-        // };
-    // }
-// }
     async signUp(id){
         const data = await this.#db.fetch(`INSERT INTO login (id) VALUES ($1) RETURNING id`,id )
         return data
