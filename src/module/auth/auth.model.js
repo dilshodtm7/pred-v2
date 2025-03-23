b7490c44bf9cfc41d545e01c6edf3d043f026d65
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2025-03-24 00:16:27 +0500

    Update auth.model.js

--- src/module/auth/auth.model.js
+++ src/module/auth/auth.model.js
@@ -20,12 +20,27 @@ export class LoginModel {
         return data;
     }
 
-    async signIn(id){
-        const data = await this.#db.fetch(`
-        SELECT * FROM login WHERE id = $1`,id)
-        return data
-    }
+async signIn(id, language) {
+    // Foydalanuvchini login jadvalidan tekshirish
+    const data = await this.#db.fetch(`
+        SELECT * FROM login WHERE id = $1`, [id]);
 
+    if (data.length > 0) {
+        // Agar foydalanuvchi login jadvalida bo'lsa, uni qaytarish
+        return data
+    } else {
+        // Agar foydalanuvchi login jadvalida topilmasa, unlogin jadvaliga qo'shish
+        await this.#db.execute(`
+            INSERT INTO unlogin (id, language) 
+            VALUES ($1, $2)`, [id, language]);
+
+        // return {
+        //     message: "User inserted into unlogin table",
+        //     id,
+        //     language
+        // };
+    }
+}
     async signUp(id){
         const data = await this.#db.fetch(`INSERT INTO login (id) VALUES ($1) RETURNING id`,id )
         return data
